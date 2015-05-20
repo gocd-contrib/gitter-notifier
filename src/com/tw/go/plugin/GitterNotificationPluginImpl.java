12864084eba1afb2cd9330d40735bc2d6a6107f7
Srini <srinivas.upadhya@gmail.com>
2015-05-20 14:14:09 +0530

    support plugin settings

--- src/com/tw/go/plugin/GitterNotificationPluginImpl.java
+++ src/com/tw/go/plugin/GitterNotificationPluginImpl.java
@@ -1,16 +1,19 @@
 package com.tw.go.plugin;
 
-import com.google.gson.GsonBuilder;
 import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
 import com.thoughtworks.go.plugin.api.GoPlugin;
 import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
 import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.GoApiRequest;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.GoApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import org.apache.commons.io.IOUtils;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.URL;
@@ -22,33 +25,125 @@ import static java.util.Arrays.asList;
 public class GitterNotificationPluginImpl implements GoPlugin {
     private static Logger LOGGER = Logger.getLoggerFor(GitterNotificationPluginImpl.class);
 
+    public static final String PLUGIN_ID = "gitter.notifier";
     public static final String EXTENSION_NAME = "notification";
     private static final List<String> goSupportedVersions = asList("1.0");
 
+    public static final String PLUGIN_SETTINGS_GET_CONFIGURATION = "go.plugin-settings.get-configuration";
+    public static final String PLUGIN_SETTINGS_GET_VIEW = "go.plugin-settings.get-view";
+    public static final String PLUGIN_SETTINGS_VALIDATE_CONFIGURATION = "go.plugin-settings.validate-configuration";
     public static final String REQUEST_NOTIFICATIONS_INTERESTED_IN = "notifications-interested-in";
     public static final String REQUEST_STAGE_STATUS = "stage-status";
 
+    public static final String GET_PLUGIN_SETTINGS = "go.processor.plugin-settings.get";
+
+    public static final String PLUGIN_SETTINGS_SERVER_BASE_URL = "server_base_url";
+    public static final String PLUGIN_SETTINGS_GITTER_TOKEN = "gitter_token";
+    public static final String PLUGIN_SETTINGS_GITTER_ROOM_ID = "gitter_room_id";
+
     public static final int SUCCESS_RESPONSE_CODE = 200;
+    public static final int NOT_FOUND_RESPONSE_CODE = 404;
     public static final int INTERNAL_ERROR_RESPONSE_CODE = 500;
+
+    private GoApplicationAccessor goApplicationAccessor;
 
     @Override
     public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
-        // ignore
+        this.goApplicationAccessor = goApplicationAccessor;
     }
 
     @Override
     public GoPluginApiResponse handle(GoPluginApiRequest goPluginApiRequest) {
-        if (goPluginApiRequest.requestName().equals(REQUEST_NOTIFICATIONS_INTERESTED_IN)) {
+        String requestName = goPluginApiRequest.requestName();
+        if (requestName.equals(PLUGIN_SETTINGS_GET_CONFIGURATION)) {
+            return handleGetPluginSettingsConfiguration();
+        } else if (requestName.equals(PLUGIN_SETTINGS_GET_VIEW)) {
+            try {
+                return handleGetPluginSettingsView();
+            } catch (IOException e) {
+                return renderJSON(500, String.format("Failed to find template: %s", e.getMessage()));
+            }
+        } else if (requestName.equals(PLUGIN_SETTINGS_VALIDATE_CONFIGURATION)) {
+            return handleValidatePluginSettingsConfiguration(goPluginApiRequest);
+        } else if (requestName.equals(REQUEST_NOTIFICATIONS_INTERESTED_IN)) {
             return handleNotificationsInterestedIn();
-        } else if (goPluginApiRequest.requestName().equals(REQUEST_STAGE_STATUS)) {
+        } else if (requestName.equals(REQUEST_STAGE_STATUS)) {
             return handleStageNotification(goPluginApiRequest);
         }
-        return null;
+        return renderJSON(NOT_FOUND_RESPONSE_CODE, null);
     }
 
     @Override
     public GoPluginIdentifier pluginIdentifier() {
-        return new GoPluginIdentifier(EXTENSION_NAME, goSupportedVersions);
+        return getGoPluginIdentifier();
+    }
+
+    private GoPluginApiResponse handleGetPluginSettingsConfiguration() {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put(PLUGIN_SETTINGS_SERVER_BASE_URL, createField("Server Base URL", null, true, false, "0"));
+        response.put(PLUGIN_SETTINGS_GITTER_TOKEN, createField("Gitter Token", null, true, false, "1"));
+        response.put(PLUGIN_SETTINGS_GITTER_ROOM_ID, createField("Gitter Room ID", null, true, false, "2"));
+        return renderJSON(SUCCESS_RESPONSE_CODE, response);
+    }
+
+    private Map<String, Object> createField(String displayName, String defaultValue, boolean isRequired, boolean isSecure, String displayOrder) {
+        Map<String, Object> fieldProperties = new HashMap<String, Object>();
+        fieldProperties.put("display-name", displayName);
+        fieldProperties.put("default-value", defaultValue);
+        fieldProperties.put("required", isRequired);
+        fieldProperties.put("secure", isSecure);
+        fieldProperties.put("display-order", displayOrder);
+        return fieldProperties;
+    }
+
+    private GoPluginApiResponse handleGetPluginSettingsView() throws IOException {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("template", IOUtils.toString(getClass().getResourceAsStream("/plugin-settings.template.html"), "UTF-8"));
+        return renderJSON(SUCCESS_RESPONSE_CODE, response);
+    }
+
+    private GoPluginApiResponse handleValidatePluginSettingsConfiguration(GoPluginApiRequest goPluginApiRequest) {
+        Map<String, Object> responseMap = (Map<String, Object>) JSONUtils.fromJSON(goPluginApiRequest.requestBody());
+        final Map<String, String> configuration = keyValuePairs(responseMap, "plugin-settings");
+        List<Map<String, Object>> response = new ArrayList<Map<String, Object>>();
+
+        validate(response, new FieldValidator() {
+            @Override
+            public void validate(Map<String, Object> fieldValidation) {
+                validateRequiredField(configuration, fieldValidation, PLUGIN_SETTINGS_SERVER_BASE_URL, "Server Base URL");
+            }
+        });
+
+        validate(response, new FieldValidator() {
+            @Override
+            public void validate(Map<String, Object> fieldValidation) {
+                validateRequiredField(configuration, fieldValidation, PLUGIN_SETTINGS_GITTER_TOKEN, "Gitter Token");
+            }
+        });
+
+        validate(response, new FieldValidator() {
+            @Override
+            public void validate(Map<String, Object> fieldValidation) {
+                validateRequiredField(configuration, fieldValidation, PLUGIN_SETTINGS_GITTER_ROOM_ID, "Gitter Room ID");
+            }
+        });
+
+        return renderJSON(SUCCESS_RESPONSE_CODE, response);
+    }
+
+    private void validate(List<Map<String, Object>> response, FieldValidator fieldValidator) {
+        Map<String, Object> fieldValidation = new HashMap<String, Object>();
+        fieldValidator.validate(fieldValidation);
+        if (!fieldValidation.isEmpty()) {
+            response.add(fieldValidation);
+        }
+    }
+
+    private void validateRequiredField(Map<String, String> configuration, Map<String, Object> fieldMap, String key, String name) {
+        if (configuration.get(key) == null || configuration.get(key).isEmpty()) {
+            fieldMap.put("key", key);
+            fieldMap.put("message", String.format("'%s' is a required field", name));
+        }
     }
 
     private GoPluginApiResponse handleNotificationsInterestedIn() {
@@ -58,7 +153,7 @@ public class GitterNotificationPluginImpl implements GoPlugin {
     }
 
     private GoPluginApiResponse handleStageNotification(GoPluginApiRequest goPluginApiRequest) {
-        Map<String, Object> dataMap = getMapFor(goPluginApiRequest);
+        Map<String, Object> dataMap = (Map<String, Object>) JSONUtils.fromJSON(goPluginApiRequest.requestBody());
 
         int responseCode = SUCCESS_RESPONSE_CODE;
         Map<String, Object> response = new HashMap<String, Object>();
@@ -69,9 +164,19 @@ public class GitterNotificationPluginImpl implements GoPlugin {
 
             String stageResult = (String) stageMap.get("result");
             if (!isEmpty(stageResult) && stageResult.equalsIgnoreCase("Failed")) {
-                String accessToken = System.getProperty("go.plugin.build.status.gitter.token");
-                String roomId = System.getProperty("go.plugin.build.status.gitter.roomId");
-                String goServer = System.getProperty("go.plugin.build.status.go-server");
+                PluginSettings pluginSettings = getPluginSettings();
+                String goServer = pluginSettings.getServerBaseURL();
+                if (isEmpty(goServer)) {
+                    goServer = System.getProperty("go.plugin.build.status.go-server");
+                }
+                String accessToken = pluginSettings.getGitterToken();
+                if (isEmpty(accessToken)) {
+                    accessToken = System.getProperty("go.plugin.build.status.gitter.token");
+                }
+                String roomId = pluginSettings.getGitterRoomId();
+                if (isEmpty(roomId)) {
+                    roomId = System.getProperty("go.plugin.build.status.gitter.roomId");
+                }
                 String stageLocator = String.format("%s/%s/%s/%s", pipelineMap.get("name"), pipelineMap.get("counter"), stageMap.get("name"), stageMap.get("counter"));
 
                 notifyGitter(accessToken, roomId, goServer, stageLocator);
@@ -96,12 +201,12 @@ public class GitterNotificationPluginImpl implements GoPlugin {
 
     void notifyGitter(String accessToken, String roomId, String goServer, String stageLocator) {
         String endpoint = String.format("https://api.gitter.im/v1/rooms/%s/chatMessages", roomId);
-        String trackbackURL = String.format("https://%s/go/pipelines/%s", goServer, stageLocator);
+        String trackbackURL = String.format("%s/go/pipelines/%s", goServer, stageLocator);
         String message = String.format("[%s](%s) failed.", stageLocator, trackbackURL);
 
         Map<String, String> payload = new HashMap<String, String>();
         payload.put("text", message);
-        String payloadJSON = getJSONFor(payload);
+        String payloadJSON = JSONUtils.toJSON(payload);
 
         LOGGER.info("Sending Gitter Notification - " + message);
 
@@ -146,16 +251,68 @@ public class GitterNotificationPluginImpl implements GoPlugin {
         return str == null || str.trim().isEmpty();
     }
 
-    private Map<String, Object> getMapFor(GoPluginApiRequest goPluginApiRequest) {
-        return (Map<String, Object>) new GsonBuilder().create().fromJson(goPluginApiRequest.requestBody(), Object.class);
+    public PluginSettings getPluginSettings() {
+        Map<String, Object> requestMap = new HashMap<String, Object>();
+        requestMap.put("plugin-id", PLUGIN_ID);
+        GoApiResponse response = goApplicationAccessor.submit(createGoApiRequest(GET_PLUGIN_SETTINGS, JSONUtils.toJSON(requestMap)));
+        if (response.responseBody() == null || response.responseBody().trim().isEmpty()) {
+            throw new RuntimeException("plugin is not configured. please provide plugin settings.");
+        }
+        Map<String, String> responseBodyMap = (Map<String, String>) JSONUtils.fromJSON(response.responseBody());
+        return new PluginSettings(responseBodyMap.get(PLUGIN_SETTINGS_SERVER_BASE_URL), responseBodyMap.get(PLUGIN_SETTINGS_GITTER_TOKEN), responseBodyMap.get(PLUGIN_SETTINGS_GITTER_ROOM_ID));
     }
 
-    private String getJSONFor(Object response) {
-        return new GsonBuilder().create().toJson(response);
+    private Map<String, String> keyValuePairs(Map<String, Object> map, String mainKey) {
+        Map<String, String> keyValuePairs = new HashMap<String, String>();
+        Map<String, Object> fieldsMap = (Map<String, Object>) map.get(mainKey);
+        for (String field : fieldsMap.keySet()) {
+            Map<String, Object> fieldProperties = (Map<String, Object>) fieldsMap.get(field);
+            String value = (String) fieldProperties.get("value");
+            keyValuePairs.put(field, value);
+        }
+        return keyValuePairs;
+    }
+
+    private GoPluginIdentifier getGoPluginIdentifier() {
+        return new GoPluginIdentifier(EXTENSION_NAME, goSupportedVersions);
+    }
+
+    private GoApiRequest createGoApiRequest(final String api, final String responseBody) {
+        return new GoApiRequest() {
+            @Override
+            public String api() {
+                return api;
+            }
+
+            @Override
+            public String apiVersion() {
+                return "1.0";
+            }
+
+            @Override
+            public GoPluginIdentifier pluginIdentifier() {
+                return getGoPluginIdentifier();
+            }
+
+            @Override
+            public Map<String, String> requestParameters() {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> requestHeaders() {
+                return null;
+            }
+
+            @Override
+            public String requestBody() {
+                return responseBody;
+            }
+        };
     }
 
     private GoPluginApiResponse renderJSON(final int responseCode, Object response) {
-        final String json = response == null ? null : getJSONFor(response);
+        final String json = response == null ? null : JSONUtils.toJSON(response);
         return new GoPluginApiResponse() {
             @Override
             public int responseCode() {
